77a6bc7ac7923ad49457432dba38139e9cdc573a
Obbie <obipras@gmail.com>
2021-09-28 12:17:49 +0700

    Final

---
+++ SurvivalShooter/Assets/Scripts/PowerUp/HealthUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUp : MonoBehaviour
+{
+    public int healthBonus = 10;
+    private PlayerHealth playerHealth;
+
+    private void Awake()
+    {
+        transform.Translate(0, 0.5f, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PowerUpCollected(other.gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime);
+    }
+
+    void PowerUpCollected(GameObject gameObjectCollectingPowerUp)
+    {
+        playerHealth = gameObjectCollectingPowerUp.GetComponent<PlayerHealth>();
+
+        // We only care if we've been collected by the player
+        if (gameObjectCollectingPowerUp.tag == "Player")
+        {
+            playerHealth.SetHealthAdjustment(healthBonus);
+
+            Debug.Log("Power Up collected, issuing payload for: " + gameObject.name);
+
+            Destroy(gameObject);
+        }        
+    }
+}
---
+++ SurvivalShooter/Assets/Scripts/PowerUp/NoDrop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoDrop : MonoBehaviour
+{
+    private void Awake()
+    {
+        Destroy(gameObject);
+    }
+}
